2e4d5220f20d3875fbbd33b4dd1fbf4c329d90ad
Claudius Mbemba <mbembac@gmail.com>
2017-11-02 12:36:34 -0700

    package updates not breaking

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("51be1678-3fe0-444d-87cd-c265080e9e25")]
+[assembly: Guid("51be1678-3fe0-444d-87cd-c265080e9e28")]
 
 
 // Version information for an assembly consists of the following four values:
